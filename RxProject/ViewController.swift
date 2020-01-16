fa371cd6210fe8c8daf2dccead3cae82ae5819c8
Ovi Bortas <ovib313@gmail.com>
2020-01-16 16:57:48 -0500

    Initial Commit

---
+++ RxProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxProject
+//
+//  Created by Ovidiu Bortas on 1/16/20.
+//  Copyright © 2020 Detroit Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
